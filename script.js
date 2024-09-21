c7a4e9edd1d33287b456f137f50cb1b920e87902
richielagito <88770508+richielagito@users.noreply.github.com>
2024-09-21 22:29:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 window.onload = function () {
-  const activePages = window.location.pathname;
-  const homeNav = document.querySelector(".home-nav");
-  const navLinks = document.querySelectorAll(".navbar a").forEach((link) => {
-    console.log(activePages);
-    if (activePages == '/') {
-      homeNav.classList.add("active");
-    } else if (link.href.includes(`${activePages}`)) {
-      link.classList.add("active");
-    } 
+  const navLinks = document.querySelectorAll(".navbar a");
+
+  navLinks.forEach(navLinkEl => {
+    navLinkEl.addEventListener('click', () => {
+      document.querySelector('.active')?.classList.remove('active');
+      navLinkEl.classList.add('active');
+    });
   });
+  
   const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
   menu_btn.addEventListener("click", function () {
