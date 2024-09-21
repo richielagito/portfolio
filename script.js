0213f77ee2eed420bd5e6c2aae3abeff5713c96e
richielagito <88770508+richielagito@users.noreply.github.com>
2024-09-21 21:56:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 window.onload = function () {
   const activePages = window.location.pathname;
-  console.log(activePages);
-  // const homeNav = document.querySelectorAll(".home-nav");
-  // const navLinks = document.querySelectorAll(".navbar a").forEach((link) => {
-  //   if (link.href.includes(`${activePages}`)) {
-  //     link.classList.add("active");
-  //   } 
-  // });
-  // const menu_btn = document.querySelector(".hamburger");
-  // const mobile_menu = document.querySelector(".mobile-nav");
-  // menu_btn.addEventListener("click", function () {
-  //   menu_btn.classList.toggle("is-active");
-  //   mobile_menu.classList.toggle("is-active");
-  // });
+  const homeNav = document.querySelectorAll(".home-nav");
+  const navLinks = document.querySelectorAll(".navbar a").forEach((link) => {
+    if (link.href == '/') {
+      homeNav.classList.add("active");
+    } else (link.href.includes(`${activePages}`)) {
+      link.classList.add("active");
+    } 
+  });
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
 };
