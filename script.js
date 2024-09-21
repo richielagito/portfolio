0555a668a82c50ed4c72d577ebc43cddb798b941
richielagito <88770508+richielagito@users.noreply.github.com>
2024-09-21 21:51:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 window.onload = function () {
   const activePages = window.location.pathname;
-  const homeNav = document.querySelectorAll(".home-nav");
-  const navLinks = document.querySelectorAll(".navbar a").forEach((link) => {
-    if (link.href.includes(`${activePages}`)) {
-      link.classList.add("active");
-    } else {
-      homeNav.classList.add("active");
-    }
-  });
-  const menu_btn = document.querySelector(".hamburger");
-  const mobile_menu = document.querySelector(".mobile-nav");
-  menu_btn.addEventListener("click", function () {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-  });
+  console.log(activePages);
+  // const homeNav = document.querySelectorAll(".home-nav");
+  // const navLinks = document.querySelectorAll(".navbar a").forEach((link) => {
+  //   if (link.href.includes(`${activePages}`)) {
+  //     link.classList.add("active");
+  //   } 
+  // });
+  // const menu_btn = document.querySelector(".hamburger");
+  // const mobile_menu = document.querySelector(".mobile-nav");
+  // menu_btn.addEventListener("click", function () {
+  //   menu_btn.classList.toggle("is-active");
+  //   mobile_menu.classList.toggle("is-active");
+  // });
 };
