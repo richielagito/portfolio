8ce20f122e85c3ccec34ef42e2a07013dc071a09
richielagito <richielagito1@gmail.com>
2025-11-10 09:15:55 +0700

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,22 +37,13 @@ const Projects = () => {
             <h2 className="text-3xl font-bold text-center mb-12 text-white">Featured Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
                 {projectData.map((project) => (
-                    <div key={project.id} className="project-card bg-white/15 rounded-lg shadow-lg overflow-hidden group flex flex-col">
+                    <div key={project.id} className="project-card border border-white/10 bg-white/15 rounded-lg shadow-lg overflow-hidden group flex flex-col">
                         <a href={project.webUrl} target="_blank" rel="noopener noreferrer" className="flex flex-col h-full">
-                            <img src={project.imageUrl} alt={project.title} className="w-full h-48 object-cover" />
+                            <img src={project.imageUrl} alt={project.title} className="w-full h-60 object-cover" />
                             <div className="p-6 flex flex-col justify-between flex-grow">
                                 <div>
                                     <h3 className="text-xl font-semibold mb-2 text-white">{project.title}</h3>
                                     <p className="text-white/70 mb-4">{project.description}</p>
-
-                                    <div className="flex flex-wrap gap-2 mb-5">
-                                        {project.stack &&
-                                            project.stack.map((tech) => (
-                                                <span key={tech} className="backdrop-blur-md border border-white/20 bg-white/15 text-white text-sm font-medium px-3 py-1 rounded-full">
-                                                    {tech}
-                                                </span>
-                                            ))}
-                                    </div>
                                 </div>
 
                                 <span className="text-white/70 transition-color group-hover:text-white duration-400 mt-auto">
